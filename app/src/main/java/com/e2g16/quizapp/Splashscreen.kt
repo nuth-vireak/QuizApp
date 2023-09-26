bed96976ccee990edd317e00c42141fde992993e
pokhengly <henglypok21012021@gmail.com>
2023-09-26 17:11:12 +0700

    finish activity_main.xml and activity_splashscreen.xml

--- app/src/main/java/com/e2g16/quizapp/Splashscreen.kt
+++ app/src/main/java/com/e2g16/quizapp/Splashscreen.kt
@@ -1,11 +1,19 @@
 package com.e2g16.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }

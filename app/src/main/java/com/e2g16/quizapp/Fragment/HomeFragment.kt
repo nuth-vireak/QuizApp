076b448ea7da2d5e7102586e3458631ea3690773
pokhengly <henglypok21012021@gmail.com>
2023-09-28 01:48:07 +0700

    Add data binding to the build.gradle.kts file, create new files categoryadaptor.kt, categoryitem.xml, categoryModelClass.kt, make changes to fragment_home.xml by adding text color and bold style to TextViews, add gravity and paddingStart to a View, add a RecyclerView to fragment_home.xml, make changes to HomeFragment.kt by importing necessary classes, adding a GridLayoutManager, binding the FragmentHomeBinding, and adding a list of categories to the RecyclerView.

--- app/src/main/java/com/e2g16/quizapp/Fragment/HomeFragment.kt
+++ app/src/main/java/com/e2g16/quizapp/Fragment/HomeFragment.kt
@@ -1,60 +1,50 @@
 package com.e2g16.quizapp.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.e2g16.quizapp.R
+import com.e2g16.quizapp.adaptor.categoryadaptor
+import com.e2g16.quizapp.databinding.FragmentHomeBinding
+import com.e2g16.quizapp.modle.categoryModelClass
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private val binding: FragmentHomeBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    private var categoryList = ArrayList<categoryModelClass>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        categoryList.add(categoryModelClass(R.drawable.scince, "Science"))
+        categoryList.add(categoryModelClass(R.drawable.englishs, "English"))
+        categoryList.add(categoryModelClass(R.drawable.englishs, "history"))
+        categoryList.add(categoryModelClass(R.drawable.mathmetic, "mathematics"))
+        binding.categoryRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        var adapter = categoryadaptor(categoryList)
+        binding.categoryRecyclerView.adapter = adapter
+        binding.categoryRecyclerView.setHasFixedSize(true)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
